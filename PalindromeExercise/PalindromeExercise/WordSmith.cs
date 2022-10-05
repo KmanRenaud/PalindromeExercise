161b6bcc0824886ef106dfc0a37ecb47a4c990f4
Kael Renaud <Kmans2272@gmail.com>
2022-10-04 19:25:25 -0500

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string pal)
+        {
+            if (pal.ToLower() == new string(pal.ToCharArray().Reverse().ToArray()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racekar", false)]
+        [InlineData("n0On", false)]
+        [InlineData("bulud", false)]
+        [InlineData("kayak", true)]
+        [InlineData("rotator", true)]
+        public void TestIfPalindrome(string pal, bool expected)
         {
-
+            //Arrange
+            var isPal = new WordSmith();
+            //Act
+            var actual = isPal.IsAPalindrome(pal);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
